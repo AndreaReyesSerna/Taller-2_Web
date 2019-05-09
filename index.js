d825192873244d00ce22a085818a875729f26b5b
AndreaReyesSerna <andrea.serna.quin@gmail.com>
2019-05-09 13:16:53 -0500

    Base de datos y se comienza a maquetar el index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express'),
-    hbs = require('express-handlebars');
-MongoClient = require('mongodb').MongoClient,
-    ObjectID = require('mongodb').ObjectID,
-    bodyParser = require('body-parser');
+var express = require('express'), 
+  hbs = require('express-handlebars');
+  MongoClient = require('mongodb').MongoClient,
+  ObjectID = require('mongodb').ObjectID,
+  bodyParser = require('body-parser');
 //npm install handlebars
 var app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 var db;
@@ -18,13 +18,23 @@ app.set('view engine', 'handlebars');
 app.use('/static', express.static('public'));
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    if (err) throw err;
+  if (err) throw err;
 
-    //Nombre de la base de datos
-    db = client.db('tienda');
-
+  //Nombre de la base de datos
+  db = client.db('tienda');
 });
 
 //Nombre de la coleccion
 var dbName = 'products';
 var dbVenta = 'ventas';
+
+app.get('/', function (req, res) {
+
+  var productos = db.collection(dbName).find();
+  
+});
+
+//npm init
+//npm install express handlebars consolidate 
+//mongo/mongod/mongodb
+//Node index
